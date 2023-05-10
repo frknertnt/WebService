47493092148b3193da140d606ff3eadbc568e555
Furkan Ertantu <furkanertantu@outlook.com>
2023-05-10 20:00:33 +0300

    Web service Test with ASP.NET
    Basic data transfer test with Soap Header

---
+++ WebServiceASPTest/index.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebServiceASPTest
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProductService.WebService1 ps = new ProductService.WebService1();
+            ps.SecurityValue = new ProductService.Security();
+            ps.SecurityValue.Username = "demo";
+            ps.SecurityValue.Password = "demo";
+
+            ps.EditProduct(new ProductService.Product()
+            {
+                ID = 3
+            });
+        }
+    }
+}
